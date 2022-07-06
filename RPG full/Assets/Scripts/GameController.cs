40b24d65cdeaa530e7d89a3b534476c5031b304d
ancgarciamo <51928175+ancgarciamo@users.noreply.github.com>
2022-07-05 22:19:03 -0500

    Gamecontroller

--- RPG full/Assets/Scripts/GameController.cs
+++ RPG full/Assets/Scripts/GameController.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text vida_actual;
+    public Text vida_maxima;
+    public GameObject boton_ataque;
+    public GameObject boton_defensa;
+    public GameObject boton_objetos;
+    public GameObject boton_atras;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
